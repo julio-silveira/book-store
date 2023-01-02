a5e78a7ce8a20d8e5c7e00c1e94a221fdb447446
julio-silveira <julioqueiroz15@gmail.com>
2023-01-02 08:46:42 -0300

    feat: added RTL to frontend

--- app/frontend/jest.config.cjs
+++ app/frontend/jest.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   testPathIgnorePatterns: ['/dist/', '/node_modules/', '/public/'],
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/index.ts', '!src/main.tsx'],
   modulePaths: ['<rootDir>/src/'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
   transform: {
     '^.+\\.tsx?$': 'babel-jest'
   }
