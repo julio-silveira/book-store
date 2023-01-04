e15cd79dcf7519712e3e43f4d69d59f18ea9b325
julio-silveira <julioqueiroz15@gmail.com>
2023-01-04 09:49:08 -0300

    feat: cretead auth service and auth module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URL),
     BooksModule,
     UsersModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
