b597d17655c32775f7623bb748f12fedf682e559
julio-silveira <julioqueiroz15@gmail.com>
2023-01-05 17:16:56 -0300

    feat: update createbookdto and book schema

--- api/src/books/schemas/book.schema.ts
+++ api/src/books/schemas/book.schema.ts
@@ -1,1 +1,21 @@
-export class Book {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type BookDocument = HydratedDocument<Book>;
+
+@Schema()
+export class Book {
+  @Prop()
+  name: string;
+
+  @Prop()
+  author: string;
+
+  @Prop()
+  genre: string;
+
+  @Prop()
+  imageUrl: string;
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
